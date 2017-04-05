5affe8c50e8d0ebd009b928d6b973f56e1adbcb7
bradrf <brad@gigglewax.com>
2017-04-04 19:35:08 -0700

    Bump version: 0.3.0 → 0.4.0

--- kubey/__init__.py
+++ kubey/__init__.py
@@ -2,4 +2,4 @@ from .kubey import Kubey  # noqa: F401
 
 __author__ = '''Brad Robel-Forrest'''
 __email__ = 'brad@bitpony.com'
-__version__ = '__version__ = '0.3.0''
+__version__ = '__version__ = '0.4.0''
