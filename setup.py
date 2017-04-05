dc3179f2e7a35e30726b0769c3c363c3dc95be2d
bradrf <brad@gigglewax.com>
2017-04-04 19:37:17 -0700

    fix failed bumpversion

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='kubey',
-    version='version='0.4.0'',
+    version='0.3.0',
     description="Simple wrapper to help find specific Kubernetes pods and containers and run asynchronous commands.",
     long_description=readme + '\n\n' + history,
     author="Brad Robel-Forrest",
