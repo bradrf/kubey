02f65b7b5c1c75c8e66e47b4b1ab70fc4beff3ec
bradrf <brad@gigglewax.com>
2017-04-04 19:33:11 -0700

    Bump version: 0.2.0 → 0.2.0

--- kubey/__init__.py
+++ kubey/__init__.py
@@ -2,4 +2,4 @@ from .kubey import Kubey  # noqa: F401
 
 __author__ = '''Brad Robel-Forrest'''
 __email__ = 'brad@bitpony.com'
-__version__ = '0.2.0'
+__version__ = '__version__ = '0.2.0''
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='kubey',
-    version='0.2.0',
+    version='version='0.2.0'',
     description="Simple wrapper to help find specific Kubernetes pods and containers and run asynchronous commands.",
     long_description=readme + '\n\n' + history,
     author="Brad Robel-Forrest",
